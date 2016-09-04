e17e2997fc5281ce3cca2a7de701655a64431fc0
Andreas Bodén <extra@Andreass-iMac.local>
2016-09-04 22:47:47 +0200

    more styling

--- src/modules/channels/components/channel.js
+++ src/modules/channels/components/channel.js
@@ -4,7 +4,7 @@ import styles from './channel.css'
 const Channel = (props) => {
   return (
     <div onClick={props.onClick.bind(this, props.data)} className={styles.className}>
-      <img className={styles.bg} src={props.data.logo} />
+      <img className={styles.bg} src={props.data.video_banner} />
       <p className={styles.title}>{props.data.name}</p>
     </div>
   )
